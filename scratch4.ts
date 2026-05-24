fa0004b790469af09dd8713f3443f061189f8ed8
Surge-Ogiemudia <pogiemudia@gmail.com>
2026-05-24 10:40:27 +0100

    chore: remove scratch files

--- scratch4.ts
+++
@@ -1,3 +0,0 @@
-import { z } from 'zod';
-const schema = z.array(z.object({ a: z.string() }));
-console.log(schema._def);
--- scratch5.ts
+++
@@ -1,5 +0,0 @@
-import { z } from 'zod';
-const schema = z.nullable(z.string());
-const opt = z.optional(z.string());
-console.log('nullable def:', Object.keys(schema._def), 'innerType:', !!schema._def.innerType);
-console.log('optional def:', Object.keys(opt._def), 'innerType:', !!opt._def.innerType);
